8d07f36d495221eb30b1fddc7cc76b281205fdb2
Phoo Pwint Latt <162186915+lattphoopwint@users.noreply.github.com>
2026-03-03 19:56:21 +0100

    Create minimalist data-driven portfolio website

---
+++ data/profile.js
@@ -0,0 +1,61 @@
+window.PORTFOLIO_DATA = {
+  profile: {
+    name: "Latt Phoo Pwint",
+    title: "Support Engineer",
+    company: "DataSunrise Inc.",
+    companyUrl: "https://www.datasunrise.com",
+    email: "phoopwintlatt@gmail.com",
+    linkedin: "https://www.linkedin.com/in/placeholder",
+    currentLocation: "Living in Budapest, Hungary",
+    origin: "Originally from Yangon, Myanmar",
+    photo: "https://picsum.photos/640/640?grayscale",
+  },
+  about:
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer ac augue volutpat, hendrerit libero id, pharetra lorem. This section is intentionally written with placeholder text so you can easily customize your profile story later.",
+  careerGoal:
+    "I am aiming to continue my career path as a cloud engineer. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec dignissim nisi vitae orci aliquet, non tristique neque posuere.",
+  experience: [
+    {
+      role: "Support Engineer",
+      organization: "DataSunrise Inc.",
+      period: "June 2023 – Present",
+      website: "https://www.datasunrise.com",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque non lacus sit amet lorem maximus porta. Add your key responsibilities, achievements, and technologies here.",
+    },
+  ],
+  education: [
+    {
+      program: "Computer Science BSc",
+      institution: "University of Debrecen, Hungary",
+      period: "Sept 2021 – Jan 2025",
+      note: "Graduated in 2025",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Use this area to describe coursework, projects, or honors.",
+    },
+    {
+      program: "Former Student",
+      institution: "University of Information Technology, Yangon",
+      period: "Dec 2019 – Feb 2021",
+      note: "Prior studies",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. You can replace this with details about your studies and activities.",
+    },
+  ],
+  volunteering: [
+    {
+      role: "Moderator",
+      organization: "Share It Forward",
+      period: "Ongoing",
+      description:
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Add community impact highlights and responsibilities here.",
+    },
+  ],
+  languages: [
+    { language: "English", level: "C1" },
+    { language: "Burmese", level: "Native" },
+    { language: "German", level: "A1.1" },
+    { language: "Japanese", level: "N4" },
+    { language: "Hungarian", level: "Beginner" },
+  ],
+};
